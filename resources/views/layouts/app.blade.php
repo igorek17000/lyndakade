d2f379a6219a4ea5af87444d1756a5ef99e45812
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-06-24 18:06:48 +0430

    support agent added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,88 @@ session(['redirectToAfterLogin' => url()->previous()]);
 
   @csrf
 
+<style>
+    .fab-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  user-select: none;
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+}
+.fab-container:hover {
+    height: 100%;
+}
 
+.fab-container:hover .sub-button:nth-child(2) {
+        transform: translateY(-80px);
+    }
+.fab-container:hover .sub-button:nth-child(3) {
+        transform: translateY(-140px);
+    }
+.fab-container:hover .sub-button:nth-child(4) {
+        transform: translateY(-200px);
+    }
+
+.fab-container:hover  .sub-button:nth-child(5) {
+        transform: translateY(-260px);
+    }
+
+.fab-container:hover  .sub-button:nth-child(6) {
+        transform: translateY(-320px);
+    }
+
+.fab-container .fab {
+    position: relative;
+    height: 70px;
+    width: 70px;
+    background-color: #465773;
+    border-radius: 50%;
+    z-index: 2000;
+}
+.fab-container .fab::before {
+      content: " ";
+      position: absolute;
+      bottom: 0; left: 0;
+      height: 35px; width: 35px;
+      background-color: inherit;
+      border-radius: 0 0 10px 0;
+      z-index: -1;
+    }
+.fab-container .fab-content {
+      display: flex;
+      align-items: center; justify-content: center;
+      height: 100%; width: 100%;
+      border-radius: 50%;
+      }
+.fab-container .fab-content  .material-icons {
+     color: white;
+     font-size: 48px;
+}
+.fab-container .sub-button {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 10px;
+    left: 10px;
+    height: 50px;
+    width: 50px;
+    background-color: royalblue;
+    border-radius: 50%;
+    transition: all .3s ease;
+    z-index:200;
+}
+.fab-container .sub-button:hover {
+      cursor: pointer;
+    }
+.fab-container .sub-button .material-icons {
+      color: white;
+      padding-top: 6px;
+    }
+    </style>
 </head>
 
 <body>
@@ -90,7 +171,35 @@ session(['redirectToAfterLogin' => url()->previous()]);
     background-image: -webkit-linear-gradient(top,#f7f7f7 0,#e5e5e5 100%);
     background-image: -o-linear-gradient(top,#f7f7f7 0,#e5e5e5 100%);
     background-image: linear-gradient(to bottom,#f7f7f7 0,#e5e5e5 100%);">
+<div class="fab-container">
+  <div class="fab shadow">
+    <div class="fab-content">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" style="-ms-transform: rotate(360deg);-webkit-transform: rotate(360deg);transform: rotate(360deg);height: 50px;" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M21 12.22C21 6.73 16.74 3 12 3c-4.69 0-9 3.65-9 9.28c-.6.34-1 .98-1 1.72v2c0 1.1.9 2 2 2h1v-6.1c0-3.87 3.13-7 7-7s7 3.13 7 7V19h-8v2h8c1.1 0 2-.9 2-2v-1.22c.59-.31 1-.92 1-1.64v-2.3c0-.7-.41-1.31-1-1.62z" fill="white"></path><circle cx="9" cy="13" r="1" fill="white"></circle><circle cx="15" cy="13" r="1" fill="white"></circle><path d="M18 11.03A6.04 6.04 0 0 0 12.05 6c-3.03 0-6.29 2.51-6.03 6.45a8.075 8.075 0 0 0 4.86-5.89c1.31 2.63 4 4.44 7.12 4.47z" fill="white"></path></svg>
+    </div>
+  </div>
+  <div class="sub-button shadow" style="
+    background-color: #0d0;
+">
+    <a href="https://wa.me/+989910510634" target="_blank" dideo-checked="true">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" fill="white"></path></svg>
+    </a>
+  </div>
+  <div class="sub-button shadow" style="
+    background-color: #0088cc;
+">
+    <a href="https://t.me/lyndakadeSupport" target="_blank" dideo-checked="true">
+      <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path id="telegram-5" d="M12,0c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.627 -5.373,-12 -12,-12Zm0,2c5.514,0 10,4.486 10,10c0,5.514 -4.486,10 -10,10c-5.514,0 -10,-4.486 -10,-10c0,-5.514 4.486,-10 10,-10Zm2.692,14.889c0.161,0.115 0.368,0.143 0.553,0.073c0.185,-0.07 0.322,-0.228 0.362,-0.42c0.435,-2.042 1.489,-7.211 1.884,-9.068c0.03,-0.14 -0.019,-0.285 -0.129,-0.379c-0.11,-0.093 -0.263,-0.12 -0.399,-0.07c-2.096,0.776 -8.553,3.198 -11.192,4.175c-0.168,0.062 -0.277,0.223 -0.271,0.4c0.006,0.177 0.125,0.33 0.296,0.381c1.184,0.354 2.738,0.847 2.738,0.847c0,0 0.725,2.193 1.104,3.308c0.047,0.139 0.157,0.25 0.301,0.287c0.145,0.038 0.298,-0.001 0.406,-0.103c0.608,-0.574 1.548,-1.461 1.548,-1.461c0,0 1.786,1.309 2.799,2.03Zm-5.505,-4.338l0.84,2.769l0.186,-1.754c0,0 3.243,-2.925 5.092,-4.593c0.055,-0.048 0.062,-0.13 0.017,-0.188c-0.045,-0.057 -0.126,-0.071 -0.188,-0.032c-2.143,1.368 -5.947,3.798 -5.947,3.798Z" fill="white"></path></svg>
+    </a>
+  </div>
+  <div class="sub-button shadow" style="
+    background-color: #B23121;
+">
+    <a href="https://lyndakade.ir/contact-us" target="_blank" dideo-checked="true">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 2.02c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 12.55l-5.992-4.57h11.983l-5.991 4.57zm0 1.288l-6-4.629v6.771h12v-6.771l-6 4.629z" fill="white"></path></svg>
+    </a>
+  </div>
 
+</div>
     @include('go-to-top-btn')
     {{-- <div class="navbar sticky-top navbar-expand-md navbar-dark bg-dark shadow-sm" style="padding: 0!important;">
       <div class="container">
