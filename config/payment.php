58ee04513da8c30824cb86b887bcbcad26cffb5c
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-07-31 02:37:09 +0430

    fixing zaringate

--- config/payment.php
+++ config/payment.php
@@ -193,7 +193,7 @@ return [
             'zaringateApiPaymentUrl' => 'https://www.zarinpal.com/pg/StartPay/:authority/ZarinGate',
             'zaringateApiVerificationUrl' => 'https://ir.zarinpal.com/pg/services/WebGate/wsdl',
 
-            'mode' => 'zaringate', // can be normal, sandbox, zaringate
+            'mode' => 'sandbox', // can be normal, sandbox, zaringate
             'merchantId' => 'd469a65e-9997-11e9-8cae-000c29344814',
             'callbackUrl' => env('APP_URL', 'http://lyndakade.ir') . '/payment/callback',
             'description' => 'پرداخت در ' . config('app.name'),
