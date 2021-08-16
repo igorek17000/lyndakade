7819cb5ecd46d5f600446f44a00bfd139170bdee
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-16 16:45:29 +0430

    fix bugs

--- resources/views/sitemaps/courses.blade.php
+++ resources/views/sitemaps/courses.blade.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8" xmlns:xhtml="http://www.w3.org/1999/xhtml"?>'; ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8" xmlns="http://www.w3.org/1999/xhtml" ?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   @foreach ($items as $item)
     <url>
