1f76ed22aba14ba7a131da8f0d8d41f045f4d26c
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-22 22:27:00 +0430

    dashboard most paid users

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -107,11 +107,11 @@
               </thead>
               <tbody>
                   @php
-                    $paids = \App\Paid::orderBy('total', 'desc')->select('*', DB::raw('count(*) as total'), DB::raw('sum(price) as totalprice'))->groupBy('user_id')->get();
+                    $paids = \App\Paid::orderBy('totalprice', 'desc')->select('*', DB::raw('count(*) as total'), DB::raw('sum(price) as totalprice'))->groupBy('user_id')->get();
                   @endphp
                   @foreach ($paids as $index => $paid)
                     <tr>
-                        <th scope="row">{{ $index }}</th>
+                        <th scope="row">{{ $index+1 }}</th>
                         <td>{{ $paid->user->username }}</td>
                         <td>{{ $paid->totalprice }}</td>
                         <td>{{ $paid->total }}</td>
