bd3800548c24dc1be6b28ac8824d68cbe3e0a075
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-23 16:33:34 +0430

    styling package page

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -8,16 +8,21 @@
   ])
 @endpush
 @section('content')
-  <div class="container">
+  <div class="container card mt-0 mt-md-5 pt-3">
     <h1>خرید اشتراک</h1>
     <p>
       با خرید اشتراک، در مدت زمان مورد نظر، میتوانید به تعداد دوره طرح، دوره هارو بصورت رایگان باز کنید و دانلود کنید.
     </p>
-    <div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center text-center">
       @foreach ($packages as $package)
         <div class="col-md-3 col-sm-6">
-          <h3>{{ $package['title'] }}</h3>
-
+          <div class="card-body" style="border: darkcyan 2px solid; border-radius: 10px; height: 300px !important;">
+            <h3 class="pt-5 pb-4">{{ $package['title'] }}</h3>
+            <p>{{ nPersian($package['days']) }} روزه</p>
+            <p>{{ nPersian($package['count']) }} دوره آموزشی</p>
+            <p>{{ nPersian(number_format($package['price'])) }} تومان</p>
+            <button class="btn btn-secondary">خرید طرح</button>
+          </div>
         </div>
       @endforeach
     </div>
