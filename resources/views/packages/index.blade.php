558b4ffb30ea19dea045e426ade80ca70e470495
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-23 17:25:15 +0430

    styling package page

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -8,6 +8,15 @@
   ])
 @endpush
 @section('content')
+  <style>
+    .w-20 {
+      -webkit-box-flex: 0;
+      -ms-flex: 0 0 20% !important;
+      flex: 0 0 20% !important;
+      max-width: 20%;
+    }
+
+  </style>
   <div class="container card mt-0 mt-md-5 pt-3">
     <h1>خرید اشتراک</h1>
     <p>
@@ -15,7 +24,7 @@
     </p>
     <div class="row d-flex justify-content-center text-center">
       @foreach ($packages as $package)
-        <div class="col-md-2 col-sm-4 mb-4">
+        <div class="w-20 col-sm-4 mb-4">
           <div class="card-body" style="border: darkcyan 2px solid; border-radius: 10px; height: 300px !important;">
             <h3 class="pt-5 pb-4">{{ $package['title'] }}</h3>
             <p>{{ nPersian($package['days']) }} روزه</p>
