d702f29de1b1c0bc7922874b33c9b446370119f6
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-24 19:05:02 +0430

    navbar is not sticky-top

--- resources/views/demands/create.blade.php
+++ resources/views/demands/create.blade.php
@@ -4,18 +4,21 @@
   'image' => 'https://lyndakade.ir/image/logo.png',
   'title' => 'لیندا کده | درخواست دوره آموزشی',
   'keywords' => get_seo_keywords() . ' , درخواست دوره آموزشی , course request, request, demand, course demand ',
-  'description' => 'برای درخواست دوره آموزشی از این صفحه اقدام فرمایید.  | ' . get_seo_description(),
+  'description' => 'برای درخواست دوره آموزشی از این صفحه اقدام فرمایید. | ' . get_seo_description(),
   ])
 @endpush
 @section('content')
   @csrf
   <div class="container">
     <div class="row justify-content-center m-4">
+      <div class="col-12">
+        <h1>فرم درخواست دوره یا مسیر آموزشی</h1>
+      </div>
       <div class="col-md-6 com-sm-12">
         <div class="card" id="card">
           <div class="card-header">
             <h5>
-              فرم درخواست دروس
+              لینک درس یا مسیر آموزشی مطابق با وبسایت لیندا باشد.
             </h5>
           </div>
           <div class="card-body">
@@ -24,13 +27,6 @@
                 <div class="flip-card-front">
                   <form id="method-2" method="POST" action="{{ route('demands.store') }}">
                     @csrf
-                    <div class="form-group row">
-                      <div class="col-md-12">
-                        <h5>
-                          لینک درس یا مسیر آموزشی مطابق با وبسایت لیندا باشد.
-                        </h5>
-                      </div>
-                    </div>
                     <div class="form-group row">
                       <label for="link" class="col-md-4 col-form-label text-md-left">لینک درس در لیندا</label>
                       <div class="col-md-6">
@@ -61,7 +57,7 @@
         <div class="card" id="card">
           <div class="card-header">
             <h5>
-              فرم درخواست دروس
+              نام درس و نام مدرس مطابق با وبسایت لیندا باشد.
             </h5>
           </div>
           <div class="card-body">
@@ -70,13 +66,6 @@
                 <div class="flip-card-front">
                   <form id="method-1" method="POST" action="{{ route('demands.store') }}">
                     @csrf
-                    <div class="form-group row">
-                      <div class="col-md-12">
-                        <h5>
-                          نام درس و نام مدرس مطابق با وبسایت لیندا باشد.
-                        </h5>
-                      </div>
-                    </div>
                     <div class="form-group row">
                       <label for="title" class="col-md-4 col-form-label text-md-left">نام درس</label>
                       <div class="col-md-6">
