b82be10dbd69e4e6b4097c4b8050ed6341abe9f0
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-24 19:08:55 +0430

    navbar is not sticky-top

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
+@push('meta.in.head')
+  @include('meta::manager',[
+  'image' => 'https://lyndakade.ir/image/logo.png',
+  'title' => 'لیندا کده | سوالات متداول',
+  'keywords' => get_seo_keywords() . ' , سوالات متداول , faq, questions, ticket, تیکت ',
+  'description' => 'مشاهده لیست سوالات متداول کاربران. | ' . get_seo_description(),
+  ])
+@endpush
 @section('content')
   <div class="container">
     <div class="row my-4 justify-content-center">
