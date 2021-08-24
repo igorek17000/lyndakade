6181009c21bafcbc1fec28cec88feaee5fd8bfa7
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-24 23:30:05 +0430

    faq

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@ session(['redirectToAfterLogin' => url()->previous()]);
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'لیندا کده') }}</title>
+  {{-- <title>{{ config('app.name', 'لیندا کده') }}</title> --}}
 
   <link href="{{ asset('image/favicon.ico') }}" rel="icon" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
