31fece189a18a9e501b4e763e402aaeba870dc80
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-30 08:20:11 +0430

    fix bugs

--- resources/views/vendor/voyager/master.blade.php
+++ resources/views/vendor/voyager/master.blade.php
@@ -20,7 +20,7 @@
   @endif
 
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
+  {{-- <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-SB27JF9C9Y"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
@@ -32,7 +32,7 @@
 
     gtag('config', 'G-SB27JF9C9Y');
   </script>
-  <meta name="google-site-verification" content="TPdR7eAXlaJ5SPRxpwWcQbG7yNX3s-DS3tLUHlOp9RY" />
+  <meta name="google-site-verification" content="TPdR7eAXlaJ5SPRxpwWcQbG7yNX3s-DS3tLUHlOp9RY" /> --}}
 
   <!-- App CSS -->
   <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}">
