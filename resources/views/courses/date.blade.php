abc17a8e5c3dc0d31c831a304bc20b5a2b9ea8c6
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-08-31 12:33:24 +0430

    use application/ld+json

--- resources/views/courses/date.blade.php
+++ resources/views/courses/date.blade.php
@@ -12,7 +12,7 @@
       "@type": "ItemList",
       "name": "{{ date_get_seo_title($coursetype) }}",
       "itemListElement": [
-        @foreach ($courses->take(5) as $course)
+        @foreach ($courses->take(10) as $course)
           {
           "@type": "ListItem",
           "position": "{{ $loop->index + 1 }}",
