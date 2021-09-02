8165f74a5a732a81b9b69ed1f227f79ea1f7add3
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-09-02 06:35:05 +0430

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -110,6 +110,9 @@ Route::get('/instructors/{slug}', 'AuthorController@show')->name('authors.show')
 // subjects & software & libraries
 Route::get('/topics/{slug}', 'HomeController@show')->name('home.show');
 Route::get('/{slug}/{$id}-0.html', function ($slug, $id) {
+    $slug = str_replace("-training-tutorials", "", $slug);
+    $slug = str_replace("-tutorials", "", $slug);
+
     $lib = \App\Library::where('slug', $slug)->orWhere('id', $id)->first();
     if ($lib) {
         return redirect()->route('home.show', [$lib->slug]);
