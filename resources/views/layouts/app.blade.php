618f7c8144c32dd4f1321b4e6b7dff580505210d
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-10-29 01:09:14 +0330

    fix bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,11 @@ session(['redirectToAfterLogin' => url()->previous()]);
 
   <link href="{{ asset('image/favicon.ico') }}" rel="icon" />
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('black/img/apple-icon.png') }}">
+  <link rel="manifest" href="/manifest.json">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
 
   @yield('script_head')
   @stack('css_head')
@@ -640,22 +645,19 @@ session(['redirectToAfterLogin' => url()->previous()]);
   </div>
 
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts.css') }}" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/icons.css') }}" />
-  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="{{ asset('css/googlefont.css') }}" rel="stylesheet" />
   <link href="{{ asset('css/font-awesome/all.css') }}" rel="stylesheet" />
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/toastr.css') }}" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/owl.carousel.css') }}" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/owl.transitions.css') }}" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/themify-icons.css') }}" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/lyndacon.css') }}" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/my-stylesheet.css') }}" />
-  <link rel="manifest" href="/manifest.json">
   <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}">
-
+  
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
