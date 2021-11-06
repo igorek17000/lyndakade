4ea301ffb35be5488b22bcf04931ed591c95ab61
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-11-06 17:39:21 +0330

    fix faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,3 +1,32 @@
+@php
+$questions = [
+    [
+        'question' => 'نحوه خرید دوره؟',
+        'answer' => 'بطور کلی دو روش برای خرید دوره وجود دارد:
+1) خرید دوره با پرداخت مستقیم: در این روش، دوره(‌های) مورد نظر خود را به
+<a href="'
+. route('cart.index') .
+'"> سبد خرید </a>
+ خود اضافه کنید، سپس به صفحه «سبد خرید» مراجعه و با استفاده دکمه «عملیات پرداخت» اقدام به پرداخت نمایید. همچنین پس از خرید دوره(ها)، می‌توانید از طریق صفحه «دوره‌های خریداری شده»، لیست تمامی دوره‌های خریداری شده‌ی خود را مشاهده نمایید. در صورتیکه در زمان پرداخت مشکلی رخ داده است، از طریق صفحه «تماس با ما» به ما اطلاع رسانی کنید تا تیم پشتیبانی بررسی شود.
+
+2) خرید دوره با استفاده از اشتراک: در این روش، می‌بایست از طریق صفحه «خرید اشتراک»، اشتراک مورد نظر خود را خریداری نمایید، سپس همانند روش اول، دوره(های) مورد نظر خود را به «سبد خرید» اضافه کرده و سپس به صفحه «سبد خرید» بروید. این‌بار در صفحه «سبد خرید» در کنار دکمه «عملیات پرداخت»، دکمه‌ی «استفاده از اشتراک» راه نیز مشاهده می‌کنید. با استفاده از دکمه «استفاده از اشتراک»، تعداد دوره‌های موجود در سبد خرید شما، از اعتبار اشتراک شما کسر می‌شود. همچنین در صفحه سبد خرید، جدولی مربوط به «تعداد دوره‌ها در سبد خرید» و «اعتبار اشتراک فعلی شما» نیز وجود دارد.
+',
+    ],
+    [
+        'question' => '',
+        'answer' => '',
+    ],
+    [
+        'question' => '',
+        'answer' => '',
+    ],
+    [
+        'question' => '',
+        'answer' => '',
+    ],
+];
+@endphp
+
 @extends('layouts.app')
 @push('meta.in.head')
   @include('meta::manager',[
@@ -6,7 +35,7 @@
   'keywords' => get_seo_keywords() . ' , سوالات متداول , faq, questions, ticket, تیکت ',
   'description' => 'مشاهده لیست سوالات متداول کاربران. | ' . get_seo_description(),
   ])
-      {{-- <script type="application/ld+json">
+  {{-- <script type="application/ld+json">
     {
       "@context": "https://schema.org",
       "@type": "FAQPage",
@@ -64,15 +93,26 @@
               <div class="card-header p-2">
                 <h5 class="mb-0">
                   <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
-                    سوال شماره یک؟
+                    نحوه خرید دوره؟
                   </button>
                 </h5>
               </div>
               <div class="collapse" data-parent="#faqExample">
                 <div class="card-body">
                   {{-- <b>Answer:</b> --}}
-                  جواب سوال شماره یک جواب سوال شماره یک جواب سوال شماره یک جواب سوال شماره یک جواب سوال شماره یک جواب سوال
-                  شماره یک جواب سوال شماره یک
+                  بطور کلی دو روش برای خرید دوره وجود دارد:
+                  1) خرید دوره با پرداخت مستقیم: در این روش، دوره(‌های) مورد نظر خود را به «سبد خرید» خود اضافه کنید، سپس
+                  به صفحه «سبد خرید» مراجعه و با استفاده دکمه «عملیات پرداخت» اقدام به پرداخت نمایید. همچنین پس از خرید
+                  دوره(ها)، می‌توانید از طریق صفحه «دوره‌های خریداری شده»، لیست تمامی دوره‌های خریداری شده‌ی خود را مشاهده
+                  نمایید. در صورتیکه در زمان پرداخت مشکلی رخ داده است، از طریق صفحه «تماس با ما» به ما اطلاع رسانی کنید تا
+                  تیم پشتیبانی بررسی شود.
+
+                  2) خرید دوره با استفاده از اشتراک: در این روش، می‌بایست از طریق صفحه «خرید اشتراک»، اشتراک مورد نظر خود
+                  را خریداری نمایید، سپس همانند روش اول، دوره(های) مورد نظر خود را به «سبد خرید» اضافه کرده و سپس به صفحه
+                  «سبد خرید» بروید. این‌بار در صفحه «سبد خرید» در کنار دکمه «عملیات پرداخت»، دکمه‌ی «استفاده از اشتراک»
+                  راه نیز مشاهده می‌کنید. با استفاده از دکمه «استفاده از اشتراک»، تعداد دوره‌های موجود در سبد خرید شما، از
+                  اعتبار اشتراک شما کسر می‌شود. همچنین در صفحه سبد خرید، جدولی مربوط به «تعداد دوره‌ها در سبد خرید» و
+                  «اعتبار اشتراک فعلی شما» نیز وجود دارد.
                 </div>
               </div>
             </div>
@@ -81,61 +121,214 @@
               <div class="card-header p-2">
                 <h5 class="mb-0">
                   <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
-                    سوال شماره دو؟
+                    اشتراک چیست؟
                   </button>
                 </h5>
               </div>
               <div class="collapse" data-parent="#faqExample">
                 <div class="card-body">
                   {{-- <b>Answer:</b> --}}
-                  جواب سوال شماره دو جواب سوال شماره دو جواب سوال شماره دو جواب سوال شماره دو جواب سوال شماره دو جواب سوال
-                  شماره دو جواب سوال شماره دو
+                  میزان اعتباری است که میتوانید در طول مدت زمان معین شده، به تعداد دوره آموزشی آن اشتراک، دوره آموزشی را
+                  بصورت رایگان دریافت نمایید.
+                  توجه داشته باشید، در صورت پایان زمان اشتراک شما، دوره‌های آموزشی باز شده، همچنان باز خواهند ماند، و قادر
+                  به دریافت دوباره آنها می‌باشید.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    نحوه خرید اشتراک؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  برای اینکار به صفحه «خرید اشتراک» مراجعه و بروی اشتراک مورد نظر خود کلیک کنید، و پس از انجام عملیات
+                  پرداخت، اشتراک انتخابی فورا برای شما فعال می‌شود.
+                  برای بررسی میزان اعتبار اشتراک، میتوانید به صفحه «سبد خرید» یا صفحه «خرید اشتراک» مراجعه نمایید.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    نحوه استفاده از اشتراک؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  برای اینکار، دوره(های) مورد نظر خود را به «سبد خرید» اضافه کرده و سپس به صفحه «سبد خرید» بروید، و در
+                  قسمت پایین جدول، کنار دکمه «عملیات پرداخت»، دکمه‌ی «استفاده از اشتراک» راه نیز مشاهده می‌کنید. با
+                  استفاده از دکمه «استفاده از اشتراک»، تعداد دوره‌های موجود در سبد خرید شما، از اعتبار اشتراک شما کسر
+                  می‌شود. همچنین در صفحه سبد خرید، جدولی مربوط به «تعداد دوره‌ها در سبد خرید» و «اعتبار اشتراک فعلی شما»
+                  نیز وجود دارد.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    استفاده از اشتراک برای خرید مسیر آموزشی؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  توجه داشته باشید، در صورت وجود مسیر آموزشی در سبد خرید، و پرداخت از طریق دکمه «استفاده از اشتراک»، در
+                  این حالت تعداد دوره‌های موجود در مسیر آموزشی، از حساب اشتراکی شما کسر می‌شود.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    چگونه اعتبار اشتراک فعلی خود را افزایش دهیم؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  پس از خرید یک اشتراک، شما قادر به خرید دوباره اشتراک نیز می‌باشید. اشتراک فعلی و اشتراک جدید می‌توانند
+                  متفاوت باشند، در این حالت، میزان اعتبار اشتراک جدید به میزان فعلی اضافه می‌گردد.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    درخواست دوره آموزشی؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  در صورتی که با استفاده از روش دوم جستجو، قادر به یافتن دوره آموزشی مورد نظر نبوده‌اید، می‌توانید پس از
+                  ورود به حساب کاربری، به صفحه «درخواست دوره» مراجعه کنید و به یکی از دو روش، درخواست دوره ارسال نمایید:
+                  1- با استفاده از نام درس و نام مدرس برای دوره آموزشی مربوطه در وبسایت لینکدین
+                  2- با استفاده از لینک دوره آموزشی مربوطه در وبسایت لینکدین
+                  توجه داشته باشید، در صورت قرار دادن دوره آموزشی مورد نظر شما در وبسایت ما، از طریق ایمیل وارد شده در
+                  حساب کاربری، به شما اطلاع رسانی خواهد شد.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    جستجوی دوره آموزشی؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  جستجوی دوره آموزشی، به دو روش زیر قابل اجرا می‌باشد:
+                  1- با استفاده از فیلد جستجو در بخش بالا صفحه: در اینجا قابلیت جستجو با «نام دوره آموزشی به زبان فارسی یا
+                  انگلیسی» و «نام مدرس» و یا «عنوان دسته» مورد نظر را جستجو نمایید.
+                  2- با استفاده از فیلد قرار داده شده در صفحه اصلی وبسایت: در اینجا میتوانید لینک دوره آموزشی مربوطه از
+                  وبسایت لینکدین را وارد کرده و دکمه ارسال را انتخاب کنید. در این حالت، شما میتوانید وضعیت موجود بودن دوره
+                  آموزشی مورد نظر را بررسی نمایید.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    چرا فیلم دوره‌ها پخش نمی‌شوند؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  بدلیل حجیم بودن فیلم‌های هر دوره، ما آنها را بصورتی فشرده سازی کرده‌ایم که، سبب کاهش حجم چشمیگیری شده
+                  است، که این موضوع باعث صرفه جویی در مدت زمان و میزان حجم مصرفی شما می‌شود، به همین دلیل فیلم‌ها توسط
+                  برنامه‌های pot player، km player ، vlc media player و سایر مدیا پلیرهایی که قابلیت نصب codec بروی آنها
+                  وجود دارد، قابل اجرا هستند.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    تخفیفات براساس میزان خرید؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  برای هر حساب کاربری، بر اساس میزان کل هزینه‌های پرداخت شده، در خریدهای بعدی میزان درصد تخفیفی نیز در نظر
+                  گرفته می‌شود که در جدول زیر قرار دارند.
+                  — جدول —
+                  توجه داشته باشید، میزان کل هزینه‌های پرداخت شده، شامل خرید دوره‌های آموزشی، خرید مسیرهای آموزشی، خرید
+                  اشتراک می‌باشد.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    آیا طرح اینترنت نیم‌بها شامل وبسایت لینداکده نیز می‌باشد؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  بله. از طریق لینک https://l2i.ir/qmn3k قابل بررسی می‌باشد.
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header p-2">
+                <h5 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+                    تا چه مدت میتوان دوره های خریداری شده را دانلود کرد؟
+                  </button>
+                </h5>
+              </div>
+              <div class="collapse" data-parent="#faqExample">
+                <div class="card-body">
+                  {{-- <b>Answer:</b> --}}
+                  با خرید هر دوره یا مسیر آموزشی، با هر یک از روش‌های موجود، آن دوره یا مسیر آموزشی بصورت دائم برای شما
+                  باز می‌باشد. در صورتی که دوره آپدیت شود، از طریق ایمیل به شما اطلاع رسانی می‌شود و همینطور آپدیت را
+                  می‌توانید رایگان دریافت کنید.
                 </div>
               </div>
             </div>
           </section>
-
-          <section class="pt-3">
-            <h3>بخش دوم</h3>
-            <div class="card">
-              <div class="card-header p-2">
-                <h5 class="mb-0">
-                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
-                    سوال شماره یک؟
-                  </button>
-                </h5>
-              </div>
-              <div class="collapse" data-parent="#faqExample">
-                <div class="card-body">
-                  {{-- <b>Answer:</b> --}}
-                  جواب سوال شماره یک جواب سوال شماره یک جواب سوال شماره یک جواب سوال شماره یک جواب سوال شماره یک جواب سوال
-                  شماره یک جواب سوال شماره یک
-                </div>
-              </div>
-            </div>
-
-            <div class="card">
-              <div class="card-header p-2">
-                <h5 class="mb-0">
-                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" aria-expanded="false">
-                    سوال شماره دو؟
-                  </button>
-                </h5>
-              </div>
-              <div class="collapse" data-parent="#faqExample">
-                <div class="card-body">
-                  {{-- <b>Answer:</b> --}}
-                  جواب سوال شماره دو جواب سوال شماره دو جواب سوال شماره دو جواب سوال شماره دو جواب سوال شماره دو جواب سوال
-                  شماره دو جواب سوال شماره دو
-                </div>
-              </div>
-            </div>
-          </section>
-
         </div>
       </div>
     </div>
   </div>
+@endsection
+@push('js')
   <script>
     $(function() {
       var idx = 1;
