1abffe7f353c6d834067b8b5251eb51e782dedeb
Hadi Zare Zadeh <zarehadi2@gmail.com>
2021-11-07 22:30:14 +0330

    fix bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,8 @@ session(['redirectToAfterLogin' => url()->previous()]);
 
     gtag('config', 'G-SB27JF9C9Y');
   </script>
+  <!-- Clarity tracking code for https://lyndakade.ir/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "97tl9nt3cb");</script><!-- Clarity tracking code for https://lyndakade.ir/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "97tl9nt3cb");</script>
+  
   <meta name="google-site-verification" content="TPdR7eAXlaJ5SPRxpwWcQbG7yNX3s-DS3tLUHlOp9RY" />
   @stack('meta.in.head')
   {{-- @include('meta::manager') --}}
@@ -41,6 +43,8 @@ session(['redirectToAfterLogin' => url()->previous()]);
 
   @yield('script_head')
   @stack('css_head')
+
+
 
   @csrf
 
