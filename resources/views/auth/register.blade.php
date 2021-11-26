b224a781882f40d64d6996b0c83a1bc36446e419
Hadi ZareZadeh <zarehadi2@gmail.com>
2021-11-26 11:42:37 +0330

    fix bug

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -93,7 +93,7 @@
               </div>
 
               <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-left">{{ __('msg.UserName') }}</label>
+                <label for="username" class="col-md-4 col-form-label text-md-left">{{ __('msg.UserName') }}</label>
 
                 <div class="col-md-8 col-lg-6">
                   <input id="username" type="text"
