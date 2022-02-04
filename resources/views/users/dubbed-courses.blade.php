4fe54d116e9a8eab92f2ebffc2215d25d7e72462
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-02-05 03:09:56 +0330

    fix bug

--- resources/views/users/dubbed-courses.blade.php
+++ resources/views/users/dubbed-courses.blade.php
@@ -8,6 +8,18 @@
   ])
 @endpush
 @push('css_head')
+  <style>
+    .show-xs {
+      display: none !important;
+    }
+
+    @media (max-width: 767px) {
+      .show-xs {
+        display: table-cell !important;
+      }
+    }
+
+  </style>
 @endpush
 @section('content')
   <div class="container">
@@ -21,10 +33,12 @@
                   <th scope="col">#</th>
                   <th scope="col">نام دوره آموزشی</th>
                   <th scope="col">مبلغ دوره</th>
-                  <th scope="col">تعداد خرید دوره</th>
-                  <th scope="col">جمع کل خرید دوره</th>
-                  <th scope="col">تعداد خرید دوره از طریق اشتراک</th>
-                  <th scope="col">جمع کل خرید دوره از طریق اشتراک</th>
+                  <th scope="col" class="hidden-xs">تعداد خرید دوره</th>
+                  <th scope="col" class="hidden-xs">جمع کل خرید دوره</th>
+                  <th scope="col" class="hidden-xs">تعداد خرید دوره از طریق اشتراک</th>
+                  <th scope="col" class="hidden-xs">جمع کل خرید دوره از طریق اشتراک</th>
+
+                  <th scope="col" class="show-xs" >جمع کل خرید دوره</th>
                 </tr>
               </thead>
               <tbody>
@@ -34,14 +48,15 @@
                     <th scope="row">{{ $loop->iteration }}</th>
                     <td><a href="{{ $course->link }}">{{ $course->title }}</a></td>
                     <td class="text-center">{{ $course->price }}</td>
-                    <td class="text-center">{{ $course->course_total_purchase }}</td>
-                    <td class="text-center">{{ $course->balance_purchase }} تومان</td>
-                    <td class="text-center">{{ $course->course_total_unlocked }}</td>
-                    <td class="text-center">{{ $course->balance_unlocked }} تومان</td>
+                    <td class="text-center hidden-xs">{{ $course->course_total_purchase }}</td>
+                    <td class="text-center hidden-xs">{{ $course->balance_purchase }} تومان</td>
+                    <td class="text-center hidden-xs">{{ $course->course_total_unlocked }}</td>
+                    <td class="text-center hidden-xs">{{ $course->balance_unlocked }} تومان</td>
+
+
+                    <td class="text-center show-xs">{{ $course->balance_unlocked + $course->balance_purchase }} تومان</td>
+
                   </tr>
-                  @php
-                    $last_iteration = $loop->iteration;
-                  @endphp
                 @endforeach
                 <tr>
                   <th scope="row" colspan="2" class="text-center">جمع کل تسویه شده</th>
