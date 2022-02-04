7f10a6d3614cde61c1453b2bbfc756a86947d15e
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-02-04 07:45:37 +0330

    user dubbed panel

--- resources/views/users/dubbed-courses.blade.php
+++ resources/views/users/dubbed-courses.blade.php
@@ -13,70 +13,78 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-12">
-        <table class="table table-striped">
-          <thead class="thead-light">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">نام دوره آموزشی</th>
-              <th scope="col">مبلغ دوره</th>
-              <th scope="col">تعداد خرید دوره</th>
-              <th scope="col">جمع کل خرید دوره</th>
-              <th scope="col">تعداد خرید دوره از طریق اشتراک</th>
-              <th scope="col">جمع کل خرید دوره از طریق اشتراک</th>
-            </tr>
-          </thead>
-          <tbody>
+        <div class="card">
+          <div class="card-body">
+            <table class="table table-striped">
+              <thead class="thead-light">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">نام دوره آموزشی</th>
+                  <th scope="col">مبلغ دوره</th>
+                  <th scope="col">تعداد خرید دوره</th>
+                  <th scope="col">جمع کل خرید دوره</th>
+                  <th scope="col">تعداد خرید دوره از طریق اشتراک</th>
+                  <th scope="col">جمع کل خرید دوره از طریق اشتراک</th>
+                </tr>
+              </thead>
+              <tbody>
 
-            @foreach ($courses as $course)
-              <tr>
-                <th scope="row">{{ $loop->iteration }}</th>
-                <td><a href="{{ $course->link }}">{{ $course->title }}</a></td>
-                <td class="text-center">{{ $course->price }}</td>
-                <td class="text-center">{{ $course->course_total_purchase }}</td>
-                <td class="text-center">{{ $course->balance_purchase }} تومان</td>
-                <td class="text-center">{{ $course->course_total_unlocked }}</td>
-                <td class="text-center">{{ $course->balance_unlocked }} تومان</td>
-              </tr>
-              @php
-                $last_iteration = $loop->iteration;
-              @endphp
-            @endforeach
-            <tr>
-              <th scope="row" colspan="2" class="text-center">جمع کل تسویه شده</th>
-              <td colspan="5" class="text-center">{{ $total_received }} تومان</td>
-            </tr>
-            <tr>
-              <th scope="row" colspan="2" class="text-center">جمع کل</th>
-              <td colspan="5" class="text-center">{{ $total_balance }} تومان</td>
-            </tr>
-          </tbody>
-        </table>
+                @foreach ($courses as $course)
+                  <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td><a href="{{ $course->link }}">{{ $course->title }}</a></td>
+                    <td class="text-center">{{ $course->price }}</td>
+                    <td class="text-center">{{ $course->course_total_purchase }}</td>
+                    <td class="text-center">{{ $course->balance_purchase }} تومان</td>
+                    <td class="text-center">{{ $course->course_total_unlocked }}</td>
+                    <td class="text-center">{{ $course->balance_unlocked }} تومان</td>
+                  </tr>
+                  @php
+                    $last_iteration = $loop->iteration;
+                  @endphp
+                @endforeach
+                <tr>
+                  <th scope="row" colspan="2" class="text-center">جمع کل تسویه شده</th>
+                  <td colspan="5" class="text-center">{{ $total_received }} تومان</td>
+                </tr>
+                <tr>
+                  <th scope="row" colspan="2" class="text-center">جمع کل</th>
+                  <td colspan="5" class="text-center">{{ $total_balance }} تومان</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
     <div class="row justify-content-center">
       <div class="col-md-12">
-        <table class="table table-striped">
-          <thead class="thead-light">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">تاریخ تسویه</th>
-              <th scope="col">مبلغ تسویه</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($invoices as $invoice)
-              <tr>
-                <th scope="row">{{ $loop->iteration }}</th>
-                <td>{{ $invoice->created_at }}</td>
-                <td>{{ $invoice->price }} تومان</td>
-              </tr>
-            @endforeach
-            <tr>
-              <th scope="row" colspan="2" class="text-center">جمع کل</th>
-              <td class="text-center">{{ $total_received }} تومان</td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="card">
+          <div class="card-body">
+            <table class="table table-striped">
+              <thead class="thead-light">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">تاریخ تسویه</th>
+                  <th scope="col">مبلغ تسویه</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($invoices as $invoice)
+                  <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td>{{ $invoice->created_at }}</td>
+                    <td>{{ $invoice->price }} تومان</td>
+                  </tr>
+                @endforeach
+                <tr>
+                  <th scope="row" colspan="2" class="text-center">جمع کل</th>
+                  <td class="text-center">{{ $total_received }} تومان</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
