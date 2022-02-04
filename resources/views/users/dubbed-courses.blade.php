3e3b120480f309ca3c3d72c2fa7c6029e7b4edfd
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-02-04 06:23:08 +0330

    user dubbed panel

--- resources/views/users/dubbed-courses.blade.php
+++ resources/views/users/dubbed-courses.blade.php
@@ -13,12 +13,43 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4">
-          {{ auth()->user()->invoices }}
+          {{ auth()->user()->courses }}
+
+        <table class="table table-striped">
+        <thead class="thead-light">
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
       </div>
       <div class="col-md-6">
-          {{ auth()->user()->courses }}
       </div>
       <div class="col-md-2">
+          {{ auth()->user()->invoices }}
       </div>
     </div>
     <div class="row">
