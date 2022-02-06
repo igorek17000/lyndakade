d1c7f0d1f35909d3131b503bfd6d9616bc04a237
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-02-06 05:21:51 +0330

    fix bug

--- resources/views/courses/index-new.blade.php
+++ resources/views/courses/index-new.blade.php
@@ -755,16 +755,16 @@
     });
 
     $(function() {
-    //   $(document).on("click", ".preview-course-button", function(t) {
+      $(document).on("click", ".preview-course-button", function(t) {
     //     video_url = t.target.getAttribute('data-src');
 
     //     $('#preview-modal-title').text(t.target.getAttribute('data-title'));
 
     //     document.querySelector('#preview-modal-body video').setAttribute('src', video_url);
     //     document.querySelector('#preview-modal-body video').play();
-    //     $('#preview-modal').modal('toggle');
+        $('#preview-modal').modal('toggle');
 
-    //   });
+      });
       $('#preview-modal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget)
         var video_url = button.data('src');
