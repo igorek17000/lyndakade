7e3596b24ef4cd610acf4ba752d1aac64199bb8b
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-02-06 03:50:41 +0330

    fix bug

--- resources/views/courses/index-new.blade.php
+++ resources/views/courses/index-new.blade.php
@@ -242,7 +242,7 @@
                 <div style="position: relative;">
                   <img class="lazyload d-inline-block" data-src="{{ fromDLHost($path->thumbnail) }}"
                     alt="مسیر آموزشی {{ $path->title }} - Image of Learn Path {{ $path->titleEng }}"
-                    style="border-radius: 5px; max-height: 143.44px;">
+                    style="border-radius: 5px; max-height: 143.44px; min-height: 143.44px;">
                   <span
                     style="width: 70px;text-align: center;position: absolute;right: 0;bottom: 0;border-radius: 3px 0 5px 0;padding: 2px 4px 0 4px;background-color: rgba(0,0,0,.8);color: #fff;">
                     {{ $path->durationHours() + ($path->durationMinutes() > 20 ? 1 : 0) }} ساعت
