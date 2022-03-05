cbcd17e3be841d865b34626d913e83a54c2b2d3f
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-03-05 21:50:06 +0330

    fix bug

--- resources/views/courses/index-new.blade.php
+++ resources/views/courses/index-new.blade.php
@@ -405,57 +405,59 @@
         دوره های آموزشی
       </h5>
       <hr style="border-top: 1px solid  #f8ba16" class="my-2">
-      <div class="card course">
-        <div class="card-horizontal py-2">
-          <div class="img-square-wrapper">
-            <img class="d-inline-block lazyload course-img"
-              data-src="{{ $course->thumbnail ? fromDLHost($course->thumbnail) : fromDLHost($course->img) }}"
-              alt="دوره آموزشی {{ $course->title }} - Image of Course {{ $course->titleEng }}">
-            <span class="course-time-state">
-              @if ($course->durationHours == 0)
-                {{ $course->durationMinutes }} دقیقه
-              @else
-                {{ $course->durationHours + ($course->durationMinutes > 40 ? 1 : 0) }} ساعت
-              @endif
-            </span>
-            @if ($course->updateDate)
-              <span class="course-update-state">
-                بروز شده
+      @foreach (\App\Course::limit(20)->get() as $course)
+        <div class="card course">
+          <div class="card-horizontal py-2">
+            <div class="img-square-wrapper">
+              <img class="d-inline-block lazyload course-img"
+                data-src="{{ $course->thumbnail ? fromDLHost($course->thumbnail) : fromDLHost($course->img) }}"
+                alt="دوره آموزشی {{ $course->title }} - Image of Course {{ $course->titleEng }}">
+              <span class="course-time-state">
+                @if ($course->durationHours == 0)
+                  {{ $course->durationMinutes }} دقیقه
+                @else
+                  {{ $course->durationHours + ($course->durationMinutes > 40 ? 1 : 0) }} ساعت
+                @endif
               </span>
-            @endif
-            @if (get_course_status_state($course->persian_subtitle_id))
-              <div class="subtitle-state persian-subtitle-img">
-                با زیرنویس فارسی
+              @if ($course->updateDate)
+                <span class="course-update-state">
+                  بروز شده
+                </span>
+              @endif
+              @if (get_course_status_state($course->persian_subtitle_id))
+                <div class="subtitle-state persian-subtitle-img">
+                  با زیرنویس فارسی
+                </div>
+              @elseif(get_course_status_state($course->english_subtitle_id))
+                <div class="subtitle-state english-subtitle-img">
+                  با زیرنویس انگلیسی
+                </div>
+              @endif
+              <div class="card-img-overlay">
+                پیش نمایش
               </div>
-            @elseif(get_course_status_state($course->english_subtitle_id))
-              <div class="subtitle-state english-subtitle-img">
-                با زیرنویس انگلیسی
-              </div>
-            @endif
-            <div class="card-img-overlay">
-              پیش نمایش
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">
+                <p class="mt-2 text-center pr-2 mb-0"
+                  style="font-size: .9rem; font-weight: 600; max-height: 43px; overflow-y: hidden;">
+                  {{ $course->title }}
+                </p>
+                <p class="text-center pl-2 mb-0"
+                  style="font-size: .9rem; font-weight: 600; max-height: 43px; overflow-y: hidden;" dir="ltr">
+                  {{ $course->titleEng }}
+                </p>
+              </h5>
+              <p class="card-text course-description">
+                {{ $course->description }}
+              </p>
             </div>
           </div>
-          <div class="card-body">
-            <h5 class="card-title">
-              <p class="mt-2 text-center pr-2 mb-0"
-                style="font-size: .9rem; font-weight: 600; max-height: 43px; overflow-y: hidden;">
-                {{ $course->title }}
-              </p>
-              <p class="text-center pl-2 mb-0"
-                style="font-size: .9rem; font-weight: 600; max-height: 43px; overflow-y: hidden;" dir="ltr">
-                {{ $course->titleEng }}
-              </p>
-            </h5>
-            <p class="card-text course-description">
-              {{ $course->description }}
-            </p>
+          <div class="card-footer border-0">
+            <small class="text-muted">Last updated 3 mins ago</small>
           </div>
         </div>
-        <div class="card-footer border-0">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-      </div>
+      @endforeach
 
 
       <div class="row">
