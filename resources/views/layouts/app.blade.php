ab9714e9c55365076a1cc3deac20fb61dfd53f67
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-03-24 00:43:20 +0430

    fix bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -689,7 +689,7 @@
           alt="لینداکده - LyndaKade - Lynda Kade - LinkedIN" style="width: 60px; height: 55px;">
         <span class="hidden-md hidden-lg">لیندا کده</span>
       </a>
-      <span class="theme-toggle hidden-md hidden-lg" style="margin-right: auto;color: #eee;">
+      {{-- <span class="theme-toggle hidden-md hidden-lg" style="margin-right: auto;color: #eee;">
         <svg class="svg-inline--fa fa-sun fa-w-16" style="font-size: 28px;margin: 10px;" aria-hidden="true"
           data-prefix="fa" data-icon="sun" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
           data-fa-i2svg="">
@@ -697,7 +697,7 @@
             d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z">
           </path>
         </svg>
-      </span>
+      </span> --}}
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -804,7 +804,7 @@
           <li class="nav-item">
             <a class="nav-link px-md-1" href="{{ route('root.contact.us') }}">تماس با ما</a>
           </li>
-          <li class="nav-item">
+          {{-- <li class="nav-item">
             <span class="theme-toggle" style="color: #eee;">
               <svg class="svg-inline--fa fa-sun fa-w-16" style="font-size: 28px;margin: 10px;" aria-hidden="true"
                 data-prefix="fa" data-icon="sun" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
@@ -814,7 +814,7 @@
                 </path>
               </svg>
             </span>
-          </li>
+          </li> --}}
         </ul>
         <form class="form-inline my-2 my-lg-0" role="search" action="{{ route('search') }}">
           <input type="search" name="q" class="form-control mr-sm-2" role="combobox" value="{{ $q ?? '' }}"
