165b43270e44eadb74c06558c6b603e8d2d0074f
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-03-31 20:53:42 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -167,26 +167,6 @@ $packages = \App\Package::get();
 @endsection
 @section('script_body')
   <script>
-    function perToEng(str) {
-      var
-        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
-        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g];
-      if (typeof str === 'string') {
-        for (var i = 0; i < 10; i++) {
-          str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
-        }
-      }
-      return str;
-    }
-
-    function engToPer(n) {
-      const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-
-      return n
-        .toString()
-        .replace(/\d/g, x => farsiDigits[x]);
-    }
-
     function numFormat(n) {
       return n
         .toString()
