4da23a5ef74319249fc517e562d15936f0fd0562
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-04-06 20:54:07 +0430

    fix bug

--- resources/views/learn_paths/index-new.blade.php
+++ resources/views/learn_paths/index-new.blade.php
@@ -78,7 +78,7 @@
   <div id="learn-path-page">
     <div class="row active">
       <div class="course col-md-2 col-sm-6 col-12 pt-md-5 text-right" style="margin-top: 1.5rem!important;">
-        <ul style="position: sticky;top: 15px;">
+        <ul {{-- style="position: sticky;top: 15px;" --}}>
           @foreach ($categories as $category)
             <li>
               <input type="radio" id="{{ $category->titleEng }}" name="category" class="cat"
