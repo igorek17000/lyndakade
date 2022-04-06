c88ed6d31fac9bdcc351bbba75907984d836d895
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-04-06 19:48:41 +0430

    fix bug

--- resources/views/learn_paths/index-new.blade.php
+++ resources/views/learn_paths/index-new.blade.php
@@ -76,70 +76,69 @@
     {{-- "> --}}
   </div>
   <div id="learn-path-page">
-    <div class="container">
-      <div class="row active">
-        <ul class="col-md-2 col-sm-6 col-12">
-          @foreach ($categories as $category)
-            <li>
-              <input type="radio" id="{{ $category->titleEng }}" name="category" class="cat"
-                data-id="{{ $category->id }}">
-              <label for="{{ $category->titleEng }}" type="radio">{{ $category->title }}</label>
-            </li>
-          @endforeach
-        </ul>
-        <div class="col-md-10 col-sm-6 col-12">
-          @foreach ($categories as $category)
-            <div id="{{ $category->slug }}" class="library-title row active">
-              <div class="col-12">
-                <h3>{{ $category->title }}</h3>
-              </div>
-              @foreach ($category->paths as $learn_path)
-                <div class="col-sm-12 col-md-6 col-lg-3 p-2">
-                  <div class="card">
-                    <a class="card-content"
-                      href="{{ route('learn.paths.show', [explode(',', $learn_path->slug)[0]]) }}">
-                      <div class="card-img">
-                        <img itemprop="image" src="#" class="lazyload card-img" style="height: 150px !important;"
-                          data-src="{{ fromDLHost($learn_path->thumbnail) }}"
-                          alt="مسیر آموزشی {{ $learn_path->title }} - Image of Learn Path {{ $learn_path->titleEng }}" />
-                      </div>
-
-                      <div class="card-img-overlay text-center">
-                        {{-- <div class="card-img-overlay text-center vertical-center"> --}}
-                        <h5 class="titlePer" style="height: 42px !important; font-size: initial;">
-                          {{ $learn_path->title }}</h5>
-                        <h5 class="titleEng" style="height: 42px !important; font-size: initial;">
-                          {{ $learn_path->titleEng }}
-                        </h5>
-                        <div style="font-size: .8rem; font-weight: 400;">
-                          زمان کل مسیر آموزشی
-                          @if ($learn_path->durationHours() > 0)
-                            {{ nPersian($learn_path->durationHours()) }}
-                            ساعت
-                          @endif
-                          @if ($learn_path->durationMinutes() > 0)
-                            {{ nPersian($learn_path->durationMinutes()) }}
-                            دقیقه
-                          @endif
-                          <p class="mb-2 mt-2">تعداد دروس
-                            {{ nPersian(count(js_to_courses($learn_path->_courses))) }}
-                          </p>
-                          {{-- <p class="mb-2 mt-2">تعداد دروس {{ nPersian(count(js_to_courses($learn_path->courses))) }}</p> --}}
-                          <del
-                            style="background-color: #6c757d;padding: 3px 4px;border-radius: 5px;">{{ nPersian($learn_path->old_price()) }}
-                            تومان</del>
-                          <span
-                            style="background-color: lightgreen;padding: 3px 4px;border-radius: 5px;">{{ nPersian($learn_path->price()) }}
-                            تومان</span>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              @endforeach
+    <div class="row active">
+      <ul class="col-md-2 col-sm-6 col-12" style="margin-top: 1.5rem!important;">
+        @foreach ($categories as $category)
+          <li>
+            <input type="radio" id="{{ $category->titleEng }}" name="category" class="cat"
+              data-id="{{ $category->id }}">
+            <label for="{{ $category->titleEng }}" type="radio">{{ $category->title }}</label>
+          </li>
+        @endforeach
+      </ul>
+      <div class="col-md-10 col-sm-6 col-12">
+        @foreach ($categories as $category)
+          <div id="cat-{{ $category->id }}" class="library-title row active"
+            @if ($loop->first) style="margin-top: 2rem!important;" @endif>
+            <div class="col-12">
+              <h3>{{ $category->title }}</h3>
             </div>
-          @endforeach
-        </div>
+            @foreach ($category->paths as $learn_path)
+              <div class="col-sm-12 col-md-6 col-lg-3 p-2">
+                <div class="card">
+                  <a class="card-content"
+                    href="{{ route('learn.paths.show', [explode(',', $learn_path->slug)[0]]) }}">
+                    <div class="card-img">
+                      <img itemprop="image" src="#" class="lazyload card-img" style="height: 150px !important;"
+                        data-src="{{ fromDLHost($learn_path->thumbnail) }}"
+                        alt="مسیر آموزشی {{ $learn_path->title }} - Image of Learn Path {{ $learn_path->titleEng }}" />
+                    </div>
+
+                    <div class="card-img-overlay text-center">
+                      {{-- <div class="card-img-overlay text-center vertical-center"> --}}
+                      <h5 class="titlePer" style="height: 42px !important; font-size: initial;">
+                        {{ $learn_path->title }}</h5>
+                      <h5 class="titleEng" style="height: 42px !important; font-size: initial;">
+                        {{ $learn_path->titleEng }}
+                      </h5>
+                      <div style="font-size: .8rem; font-weight: 400;">
+                        زمان کل مسیر آموزشی
+                        @if ($learn_path->durationHours() > 0)
+                          {{ nPersian($learn_path->durationHours()) }}
+                          ساعت
+                        @endif
+                        @if ($learn_path->durationMinutes() > 0)
+                          {{ nPersian($learn_path->durationMinutes()) }}
+                          دقیقه
+                        @endif
+                        <p class="mb-2 mt-2">تعداد دروس
+                          {{ nPersian(count(js_to_courses($learn_path->_courses))) }}
+                        </p>
+                        {{-- <p class="mb-2 mt-2">تعداد دروس {{ nPersian(count(js_to_courses($learn_path->courses))) }}</p> --}}
+                        <del
+                          style="background-color: #6c757d;padding: 3px 4px;border-radius: 5px;">{{ nPersian($learn_path->old_price()) }}
+                          تومان</del>
+                        <span
+                          style="background-color: lightgreen;padding: 3px 4px;border-radius: 5px;">{{ nPersian($learn_path->price()) }}
+                          تومان</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            @endforeach
+          </div>
+        @endforeach
       </div>
     </div>
   </div>
