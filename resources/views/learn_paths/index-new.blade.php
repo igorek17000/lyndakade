10bda57e89027104761ede17033b50df7925ca91
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-04-06 19:17:46 +0430

    fix bug

--- resources/views/learn_paths/index-new.blade.php
+++ resources/views/learn_paths/index-new.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @push('meta.in.head')
-  @include('meta::manager',[
-  'image' => 'https://lyndakade.ir/image/logo.png',
-  'title' => 'مسیرهای آموزشی - لیندا کده',
-  'keywords' => get_seo_keywords() . ' , لیست مسیرهای آموزشی , learn path, learn-path, all learn paths ',
-  'description' => 'لیست مسیرهای آموزشی | ' . get_seo_description(),
+  @include('meta::manager', [
+      'image' => 'https://lyndakade.ir/image/logo.png',
+      'title' => 'مسیرهای آموزشی - لیندا کده',
+      'keywords' => get_seo_keywords() . ' , لیست مسیرهای آموزشی , learn path, learn-path, all learn paths ',
+      'description' => 'لیست مسیرهای آموزشی | ' . get_seo_description(),
   ])
   <script type="application/ld+json">
     {
@@ -47,21 +47,21 @@
             مسیر خود را پیدا کنید. سفر خود را شروع کنید.
           </p>
 
-          <div class="dropdown" dir="ltr">
+          {{-- <div class="dropdown" dir="ltr">
             <button class="btn btn-dark btn-lg filter-paths" type="button" id="dropdownFilter" data-toggle="dropdown"
               aria-haspopup="true" aria-expanded="false">
               فیلتربندی
             </button>
             <div class="dropdown-menu dropdown-menu-center" aria-labelledby="dropdownFilter">
-              <span class="dropdown-item {{ $selected_library == 'all' ? ' active' : '' }}" id="dropdown-see-all"
+              <span class="dropdown-item {{ $selected_category_id == -1 ? ' active' : '' }}" id="dropdown-see-all"
                 onclick="changePaths('see-all')">همه</span>
-              @foreach ($libraries as $library)
-                <span class="dropdown-item {{ $selected_library == $library->slug ? ' active' : '' }}"
-                  id="dropdown-{{ $library->slug }}"
-                  onclick="changePaths('{{ $library->slug }}')">{{ $library->title }}</span>
+              @foreach ($categories as $category)
+                <span class="dropdown-item {{ $selected_category_id == $category->id ? ' active' : '' }}"
+                  id="dropdown-{{ $category->id }}"
+                  onclick="changePaths('{{ $category->id }}')">{{ $category->title }}</span>
               @endforeach
             </div>
-          </div>
+          </div> --}}
         </div>
       </div>
     </div>
@@ -77,14 +77,66 @@
   </div>
   <div id="learn-path-page">
     <div class="container">
-      @foreach ($libraries as $library)
-        <div id="{{ $library->slug }}" class="library-title row active">
-          <div class="col-12">
-            <h3>{{ $library->title }}</h3>
-          </div>
-        @include('learn_paths.partials.list_item_grid_total', ['paths' => $library->paths])
+      <div class="row active">
+        <ul class="col-md-4 col-sm-6 col-12">
+          @foreach ($categories as $category)
+            <li>{{ $category->title }}</li>
+          @endforeach
+        </ul>
+        <div class="col-md-8 col-sm-6 col-12">
+          @foreach ($categories as $category)
+            <div id="{{ $category->slug }}" class="library-title row active">
+              <div class="col-12">
+                <h3>{{ $category->title }}</h3>
+              </div>
+              @foreach ($category->paths as $learn_path)
+                <div class="col-sm-12 col-md-6 col-lg-3 p-2">
+                  <div class="card">
+                    <a class="card-content"
+                      href="{{ route('learn.paths.show', [explode(',', $learn_path->slug)[0]]) }}">
+                      <div class="card-img">
+                        <img itemprop="image" src="#" class="lazyload card-img" style="height: 150px !important;"
+                          data-src="{{ fromDLHost($learn_path->thumbnail) }}"
+                          alt="مسیر آموزشی {{ $learn_path->title }} - Image of Learn Path {{ $learn_path->titleEng }}" />
+                      </div>
+
+                      <div class="card-img-overlay text-center">
+                        {{-- <div class="card-img-overlay text-center vertical-center"> --}}
+                        <h5 class="titlePer" style="height: 42px !important; font-size: initial;">
+                          {{ $learn_path->title }}</h5>
+                        <h5 class="titleEng" style="height: 42px !important; font-size: initial;">
+                          {{ $learn_path->titleEng }}
+                        </h5>
+                        <div style="font-size: .8rem; font-weight: 400;">
+                          زمان کل مسیر آموزشی
+                          @if ($learn_path->durationHours() > 0)
+                            {{ nPersian($learn_path->durationHours()) }}
+                            ساعت
+                          @endif
+                          @if ($learn_path->durationMinutes() > 0)
+                            {{ nPersian($learn_path->durationMinutes()) }}
+                            دقیقه
+                          @endif
+                          <p class="mb-2 mt-2">تعداد دروس
+                            {{ nPersian(count(js_to_courses($learn_path->_courses))) }}
+                          </p>
+                          {{-- <p class="mb-2 mt-2">تعداد دروس {{ nPersian(count(js_to_courses($learn_path->courses))) }}</p> --}}
+                          <del
+                            style="background-color: #6c757d;padding: 3px 4px;border-radius: 5px;">{{ nPersian($learn_path->old_price()) }}
+                            تومان</del>
+                          <span
+                            style="background-color: lightgreen;padding: 3px 4px;border-radius: 5px;">{{ nPersian($learn_path->price()) }}
+                            تومان</span>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              @endforeach
+            </div>
+          @endforeach
         </div>
-      @endforeach
+      </div>
     </div>
   </div>
 @endsection
