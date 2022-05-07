74397d389a7753e29f4e2006ca0301b268427716
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-07 08:00:54 +0430

    fix bug

--- resources/views/sitemaps/courses.blade.php
+++ resources/views/sitemaps/courses.blade.php
@@ -1,24 +1,17 @@
-@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php echo '<?xml version="1.0" encoding="UTF-8"@endphp'; ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   @foreach ($items as $item)
     <url>
       <loc>{{ route($route_name, $item->slug_linkedin) }}</loc>
       @php
         echo "<video:video>
-            <video:thumbnail_loc>" .
-            fromDLHost($item->img) .
-            "</video:thumbnail_loc>
+            <video:thumbnail_loc>$item->img</video:thumbnail_loc>
             <video:title>$item->title</video:title>
             <video:description>$item->description</video:description>
-            <video:publication_date>" .
-            \Carbon\Carbon::parse($item->releaseDate) .
-            "</video:publication_date>
-            <video:content_loc>" .
-            fromDLHost($item->previewFile) .
-            "</video:content_loc>
-            <video:duration>" .
-            ($item->durationHours * 60 + $item->durationMinutes) * 60 .
-            "</video:duration>
+            <video:publication_date>$item->releaseDate</video:publication_date>
+            <video:content_loc>$item->previewFile</video:content_loc>
+            <video:duration>$item->duration</video:duration>
             <video:tag>دانلود دوره آموزشی از لینکدین رایگان</video:tag>
             <video:tag>دانلود دوره آموزشی از لیندا رایگان</video:tag>
             <video:tag>دانلود دوره آموزشی رایگان</video:tag>
