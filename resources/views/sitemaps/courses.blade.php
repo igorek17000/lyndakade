1671a35621c1991f387124d9ffae0d9f8bdf907e
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-07 07:57:02 +0430

    fix bug

--- resources/views/sitemaps/courses.blade.php
+++ resources/views/sitemaps/courses.blade.php
@@ -3,7 +3,7 @@
   @foreach ($items as $item)
     <url>
       <loc>{{ route($route_name, $item->slug_linkedin) }}</loc>
-      @php
+      {{-- @php
         echo "<video:video>
             <video:thumbnail_loc>" .
             fromDLHost($item->img) .
@@ -25,7 +25,7 @@
             <video:tag>لینداکده</video:tag>
             </video:video>
             ";
-      @endphp
+      @endphp --}}
       <lastmod>{{ $today_date }}</lastmod>
       <changefreq>daily</changefreq>
       <priority>{{ $priority }}</priority>
