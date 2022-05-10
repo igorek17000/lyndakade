6bed3eb23a7037ff5dbcc11fb8480f7b73f94f1f
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-10 06:41:33 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -78,11 +78,11 @@
                 style=" position: absolute; z-index: 10; left: 8px; top: 7px; font-size: 18px;"></i></span>
             <input readonly=""
               onclick="(()=>{this.select();
-                                                                                                                                                                            this.setSelectionRange(0, 99999);
-                                                                                                                                                                            navigator.clipboard.writeText(this.value);
-                                                                                                                                                                              toastr.options.rtl = true;
-                                                                                                                                                                              toastr.options.positionClass = 'toast-bottom-left';
-                                                                                                                                                                            toastr.info('لینک کوتاه کپی شد.');})()"
+                                                                                                                                                                              this.setSelectionRange(0, 99999);
+                                                                                                                                                                              navigator.clipboard.writeText(this.value);
+                                                                                                                                                                                toastr.options.rtl = true;
+                                                                                                                                                                                toastr.options.positionClass = 'toast-bottom-left';
+                                                                                                                                                                              toastr.info('لینک کوتاه کپی شد.');})()"
               style=" font-size: 12px; text-align: left; direction: rtl; padding-left: 27px; padding-right: 2px; "
               title="لینک کوتاه این دوره" type="text" value="lyndakade.ir/C/{{ $course->id }}" id="shorturl"
               class="form-control">
@@ -92,7 +92,7 @@
 
         <div class="video-player">
           <video class="video-js w-100 vjs-theme-city" controls preload="auto" poster="{{ fromDLHost($course->img) }}"
-            {{-- crossorigin="anonymous" --}} {{-- data-setup='{ "fluid" : true , "controls": true, "autoplay": true, "preload": "auto", "seek": true  }' --}}
+            width="100%" {{-- crossorigin="anonymous" --}} {{-- data-setup='{ "fluid" : true , "controls": true, "autoplay": true, "preload": "auto", "seek": true  }' --}}
             data-setup='{ "controls": true, "autoplay": true, "preload": "auto", "seek": true  }'>
             <source type="video/mp4" src="{{ fromDLHost($course->previewFile) }}" />
 
@@ -168,9 +168,9 @@
                 @endif
                 <div class="author-thumb">
                   <div style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                                font-family: inherit;
-                                                                font-weight: 500;
-                                                                line-height: 1.2;margin-top: 0;">مدرس</div>
+                                                                  font-family: inherit;
+                                                                  font-weight: 500;
+                                                                  line-height: 1.2;margin-top: 0;">مدرس</div>
                   @foreach ($course->authors as $author)
                     <a href="{{ route('authors.show', [$author->slug]) }}">
                       <img src="#" class="lazyload" width="100" height="100"
@@ -185,9 +185,9 @@
                   <div style="background-color: #ece81a;padding: 10px 0;border-radius: 15px;margin-top: 5px;"
                     class="author-thumb">
                     <div style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                                font-family: inherit;
-                                                                font-weight: 500;
-                                                                line-height: 1.2;margin-top: 0;">دوبله کننده</div>
+                                                                  font-family: inherit;
+                                                                  font-weight: 500;
+                                                                  line-height: 1.2;margin-top: 0;">دوبله کننده</div>
                     @foreach ($course->users as $user)
                       <a href="{{ route('dubbed.index', [$user->username]) }}">
                         <img src="#" class="lazyload" alt="عکس {{ $user->name }} - Image of {{ $user->name }}"
@@ -433,9 +433,9 @@
                 @endif
                 <div class="author-thumb">
                   <div style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                            font-family: inherit;
-                                                            font-weight: 500;
-                                                            line-height: 1.2;margin-top: 0;">Author</div>
+                                                              font-family: inherit;
+                                                              font-weight: 500;
+                                                              line-height: 1.2;margin-top: 0;">Author</div>
                   @foreach ($course->authors as $author)
                     <a href="{{ route('authors.show', [$author->slug]) }}">
                       <img src="#" class="lazyload" width="100" height="100"
@@ -450,9 +450,9 @@
                   <div style="background-color: #ece81a;padding: 10px 0;border-radius: 15px;margin-top: 5px;"
                     class="author-thumb">
                     <div style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                              font-family: inherit;
-                                                              font-weight: 500;
-                                                              line-height: 1.2;margin-top: 0;">Dubbed By</div>
+                                                                font-family: inherit;
+                                                                font-weight: 500;
+                                                                line-height: 1.2;margin-top: 0;">Dubbed By</div>
                     @foreach ($course->users as $user)
                       <a href="{{ route('dubbed.index', [$user->username]) }}">
                         <img src="#" class="lazyload" alt="عکس {{ $user->name }} - Image of {{ $user->name }}"
