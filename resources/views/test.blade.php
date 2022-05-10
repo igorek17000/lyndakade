805a592e83f887e027901c88f8378efc028566c3
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-10 07:43:22 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -93,7 +93,7 @@
         <div class="video-player">
           <video class="video-js vjs-theme-city" controls preload="auto" poster="{{ fromDLHost($course->img) }}"
             width="920" height="720" {{-- crossorigin="anonymous" --}} {{-- data-setup='{ "fluid" : true , "controls": true, "autoplay": true, "preload": "auto", "seek": true  }' --}}
-            data-setup='{ "controls": true, "autoplay": true, "preload": "auto", "seek": true  }'>
+            data-setup='{ "controls": true, "autoplay": true, "preload": "auto", "seek": true  }' style="max-width: 100%; max-height: 100%;">
             <source type="video/mp4" src="{{ fromDLHost(str_replace('preview', 'preview1', $course->previewFile)) }}" />
 
             {{-- @if ($course->previewSubtitle)
