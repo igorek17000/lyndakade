a76b89b69d8aec3708732ec05d3ed7fc0ae5383c
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-12 17:15:48 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -100,14 +100,14 @@
           <video playsinline id="plyr-video" poster="{{ fromDLHost($course->img) }}" controls>
             <source type="video/mp4" src="{{ fromDLHost(str_replace('preview', 'preview1', $course->previewFile)) }}"
               size="720" />
-            {{-- <track kind="captions" label="English captions"
-              src="{{ str_replace('https:', '', fromDLHost($course->previewSubtitle)) }}" srclang="en" default> --}}
+            <track kind="captions" label="English captions"
+              src="{{ str_replace('https:', '', fromDLHost($course->previewSubtitle)) }}" srclang="en" default>
 
-            @if ($course->previewSubtitleContent)
+            {{-- @if ($course->previewSubtitleContent)
               @php echo '<script type="text/vtt">'; @endphp
-                                                          {{ $course->previewSubtitleContent }}
-                                                      @php echo '</script>'; @endphp
-            @endif
+                {{ $course->previewSubtitleContent }}
+            @php echo '</script>'; @endphp
+            @endif --}}
           </video>
         </div>
         <nav>
