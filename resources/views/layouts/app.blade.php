01fe47dd19534b3450a63ffde90e512a4e28f2f1
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-12 20:56:35 +0430

    fix bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,6 +106,9 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('css/icons.css') }}" />
   <link rel="stylesheet" type="text/css" href="{{ asset('css/my-stylesheet.css') }}" />
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/plyr/3.7.2/plyr.css"
+    integrity="sha512-SwLjzOmI94KeCvAn5c4U6gS/Sb8UC7lrm40Wf+B0MQxEuGyDqheQHKdBmT4U+r+LkdfAiNH4QHrHtdir3pYBaw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   {{-- <script async src="{{ asset('js/lazysizes.min.js') }}"></script> --}}
   <script async src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"
     integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
