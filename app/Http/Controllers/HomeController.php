8c8ec1c8315571975c6a2d5039ca225a7ad69501
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-05-15 14:58:37 +0430

    fix bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -535,9 +535,8 @@ class HomeController extends Controller
             $file_path = str_replace(".mp4", ".vtt", $course->previewFile);
             if ($lang != 'fa')
                 $file_path = str_replace(".mp4", ".en.vtt", $course->previewFile);
-
+            dd($file_path);
             $content = Storage::disk('FTP')->get($file_path);
-            dd($content);
             if (strpos(strtolower("WEBVTT"), strtolower($content)) != false)
                 return $content;
             return '';
