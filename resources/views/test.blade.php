da898019182be83e16b54a34bd7fd2db148b9bfa
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-06-06 10:20:25 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -210,10 +210,10 @@ if (count($course->subjects) > 0) {
     }
 
     /* @media (max-width: 768px) {
-                                          :root {
-                                            --sider-size: 100vw;
-                                          }
-                                        } */
+                                            :root {
+                                              --sider-size: 100vw;
+                                            }
+                                          } */
 
     #dismiss {
       width: 30px;
@@ -357,8 +357,8 @@ if (count($course->subjects) > 0) {
     }
 
     /* ---------------------------------------------------
-                                                                        CONTENT STYLE
-                                                                    ----------------------------------------------------- */
+                                                                          CONTENT STYLE
+                                                                      ----------------------------------------------------- */
 
     #content {
       width: 100%;
@@ -416,10 +416,10 @@ if (count($course->subjects) > 0) {
     @media (max-width: 767px) {
 
       /* #sidebarCollapse.active span {
-                                                    transform: none;
-                                                    opacity: 1;
-                                                    margin: 5px auto;
-                                                  } */
+                                                      transform: none;
+                                                      opacity: 1;
+                                                      margin: 5px auto;
+                                                    } */
 
       #sidebar {
         margin-right: calc(var(--sidebar-size)*(-1));
@@ -446,10 +446,10 @@ if (count($course->subjects) > 0) {
 
     @media (min-width: 768px) {
       /* #sidebarCollapse span {
-                                                    transform: none;
-                                                    opacity: 1;
-                                                    margin: 5px auto;
-                                                  } */
+                                                      transform: none;
+                                                      opacity: 1;
+                                                      margin: 5px auto;
+                                                    } */
 
       .overlay.active {
         display: none;
@@ -638,9 +638,9 @@ if (count($course->subjects) > 0) {
                 <div class="author-thumb">
                   <div
                     style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                                                                                                                                                  font-family: inherit;
-                                                                                                                                                                                  font-weight: 500;
-                                                                                                                                                                                  line-height: 1.2;margin-top: 0;">
+                                                                                                                                                                                    font-family: inherit;
+                                                                                                                                                                                    font-weight: 500;
+                                                                                                                                                                                    line-height: 1.2;margin-top: 0;">
                     مدرس
                   </div>
                   @foreach ($course->authors as $author)
@@ -658,9 +658,9 @@ if (count($course->subjects) > 0) {
                     class="author-thumb">
                     <div
                       style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                                                                                                                                                  font-family: inherit;
-                                                                                                                                                                                  font-weight: 500;
-                                                                                                                                                                                  line-height: 1.2;margin-top: 0;">
+                                                                                                                                                                                    font-family: inherit;
+                                                                                                                                                                                    font-weight: 500;
+                                                                                                                                                                                    line-height: 1.2;margin-top: 0;">
                       دوبلور
                     </div>
                     @foreach ($course->users as $user)
@@ -1202,6 +1202,13 @@ if (count($course->subjects) > 0) {
     // });
 
     $(document).ready(function() {
+      $('.carousel').carousel({
+        interval: false,
+        wrap: false,
+        keyboard: false,
+      });
+    });
+    // $(document).ready(function() {
       var course_id = '{{ $course->id }}';
       var course_title = '{{ $course->title }}';
       var course_titleEng = '{{ $course->titleEng }}';
@@ -1214,11 +1221,6 @@ if (count($course->subjects) > 0) {
           });
           isSent = true;
         }
-      });
-      $('.carousel').carousel({
-        interval: false,
-        wrap: false,
-        keyboard: false,
       });
 
       $(document).on('click', '#dismiss, .overlay', function(event) {
@@ -1273,6 +1275,6 @@ if (count($course->subjects) > 0) {
         // };
       });
       $('#sidebarCollapse').click();
-    });
+    // });
   </script>
 @endsection
