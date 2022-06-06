0d20452231cb2685f18c972b1e4a593754fc1c4d
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-06-06 07:28:48 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -210,10 +210,10 @@ if (count($course->subjects) > 0) {
     }
 
     /* @media (max-width: 768px) {
-      :root {
-        --sider-size: 100vw;
-      }
-    } */
+        :root {
+          --sider-size: 100vw;
+        }
+      } */
 
     #dismiss {
       width: 35px;
@@ -357,8 +357,8 @@ if (count($course->subjects) > 0) {
     }
 
     /* ---------------------------------------------------
-                                    CONTENT STYLE
-                                ----------------------------------------------------- */
+                                      CONTENT STYLE
+                                  ----------------------------------------------------- */
 
     #content {
       width: 100%;
@@ -415,10 +415,10 @@ if (count($course->subjects) > 0) {
     @media (max-width: 768px) {
 
       /* #sidebarCollapse.active span {
-                transform: none;
-                opacity: 1;
-                margin: 5px auto;
-              } */
+                  transform: none;
+                  opacity: 1;
+                  margin: 5px auto;
+                } */
 
       #sidebar {
         margin-right: calc(var(--sidebar-size)*(-1));
@@ -443,10 +443,10 @@ if (count($course->subjects) > 0) {
 
     @media (min-width: 769px) {
       /* #sidebarCollapse span {
-                transform: none;
-                opacity: 1;
-                margin: 5px auto;
-              } */
+                  transform: none;
+                  opacity: 1;
+                  margin: 5px auto;
+                } */
 
       .overlay.active {
         display: none;
@@ -469,6 +469,9 @@ if (count($course->subjects) > 0) {
 
     <nav id="sidebar">
       <ul class="list-unstyled components">
+        <div class="dismiss">
+          <i class="fas fa-arrow-left"></i>
+        </div>
         <p>سرفصل‌ها</p>
         <li>
           <a href="#chap1" title="0. Introduction" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
@@ -628,9 +631,9 @@ if (count($course->subjects) > 0) {
                     <div class="author-thumb">
                       <div
                         style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                                                                                                              font-family: inherit;
-                                                                                                                                              font-weight: 500;
-                                                                                                                                              line-height: 1.2;margin-top: 0;">
+                                                                                                                                                font-family: inherit;
+                                                                                                                                                font-weight: 500;
+                                                                                                                                                line-height: 1.2;margin-top: 0;">
                         مدرس
                       </div>
                       @foreach ($course->authors as $author)
@@ -648,9 +651,9 @@ if (count($course->subjects) > 0) {
                         class="author-thumb">
                         <div
                           style="font-size: 1.25rem;margin-bottom: 0.5rem;
-                                                                                                                                              font-family: inherit;
-                                                                                                                                              font-weight: 500;
-                                                                                                                                              line-height: 1.2;margin-top: 0;">
+                                                                                                                                                font-family: inherit;
+                                                                                                                                                font-weight: 500;
+                                                                                                                                                line-height: 1.2;margin-top: 0;">
                           دوبلور
                         </div>
                         @foreach ($course->users as $user)
