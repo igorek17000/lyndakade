d2dd308a5662e7b79b52defd6c84c8d7db0fd8d7
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-06-08 01:09:20 +0430

    fix bug

--- app/Http/Controllers/VoyagerCourseController.php
+++ app/Http/Controllers/VoyagerCourseController.php
@@ -67,7 +67,7 @@ class VoyagerCourseController extends \TCG\Voyager\Http\Controllers\VoyagerBaseC
             $chapters_file = $course_path . "/chapters.json";
             $chapters_file_content = Storage::disk('FTP')->get($chapters_file);
             if (strlen($chapters_file_content) > 0) {
-                $chapters = json_decode($chapters_file_content)['chapters'];
+                $chapters = json_decode($chapters_file_content)->chapters;
                 foreach ($chapters as $chapter) {
                     foreach ($chapter->videos as $video) {
                         $video->id = create_hashed_data_if_not_exists($course->id . "-" . $video->index);
