ee4073df43f50c6c4ac7b45c170b0a4f720c03d4
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-06-08 00:00:21 +0430

    fix bug

--- app/Http/Controllers/VoyagerCourseController.php
+++ app/Http/Controllers/VoyagerCourseController.php
@@ -63,7 +63,7 @@ class VoyagerCourseController extends \TCG\Voyager\Http\Controllers\VoyagerBaseC
         $videos = "[]";
         try {
             $previewFile = json_decode($course->previewFile)[0]->download_link;
-            $course_path = str_replace("/preview.mp4", "", strtolower($previewFile));
+            $course_path = str_replace("/preview.mp4", "", $previewFile);
             $chapters_file = "/public_html" . "/" . $course_path . "/chapters.json";
             $chapters_file_content = Storage::disk('FTP')->get($chapters_file);
             if (strlen($chapters_file_content) > 0) {
