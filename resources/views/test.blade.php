2c99633dabd89d4593294a899d48112d4ff8a3fc
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-06-16 22:57:22 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -422,22 +422,22 @@ if (count($course->subjects) > 0) {
     }
 
     /* #sidebarCollapse.active .sidebarCollapse span:first-of-type {
-                                                                              transform: rotate(45deg) translate(2px, 2px);
-                                                                            }
+                                                                                transform: rotate(45deg) translate(2px, 2px);
+                                                                              }
 
-                                                                            #sidebarCollapse.active .sidebarCollapse span:nth-of-type(2) {
-                                                                              opacity: 0;
-                                                                            }
+                                                                              #sidebarCollapse.active .sidebarCollapse span:nth-of-type(2) {
+                                                                                opacity: 0;
+                                                                              }
 
-                                                                            #sidebarCollapse.active .sidebarCollapse span:last-of-type {
-                                                                              transform: rotate(-45deg) translate(1px, -1px);
-                                                                            }
+                                                                              #sidebarCollapse.active .sidebarCollapse span:last-of-type {
+                                                                                transform: rotate(-45deg) translate(1px, -1px);
+                                                                              }
 
-                                                                            #sidebarCollapse.active .sidebarCollapse span {
-                                                                              transform: none;
-                                                                              opacity: 1;
-                                                                              margin: 0 auto;
-                                                                            } */
+                                                                              #sidebarCollapse.active .sidebarCollapse span {
+                                                                                transform: none;
+                                                                                opacity: 1;
+                                                                                margin: 0 auto;
+                                                                              } */
 
     @media (max-width: 767px) {
 
@@ -822,7 +822,13 @@ if (count($course->subjects) > 0) {
                     </div>
                   </li>
                   <li>
-                  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                    <svg class="svg-inline--fa fa-exclamation-triangle fa-w-18" aria-hidden="true" data-prefix="fa"
+                      data-icon="exclamation-triangle" role="img" xmlns="http://www.w3.org/2000/svg"
+                      data-fa-i2svg="" style="float: right;margin-left: 7px;font-size: 20px;" viewBox="0 0 576 512">
+                      <path fill="currentColor"
+                        d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z">
+                      </path>
+                    </svg>
                     <div>
                       <span class="dot-delimiter-after">گزارش خرابی</span>
                       <button class="btn-link report-issue-toggle">
