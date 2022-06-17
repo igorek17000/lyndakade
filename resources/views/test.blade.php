10440d46d8bc0600df2d3e5400b7e7a0811179dd
Hadi ZareZadeh <zarehadi2@gmail.com>
2022-06-17 16:06:03 +0430

    fix bug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1684,10 +1684,11 @@ if (count($course->subjects) > 0) {
       var cTime = course_player.currentTime;
       var idx = 0;
       document.querySelectorAll(`.content-transcript-line[data-index-${course_player.captions.language}]`).forEach(el=>{
+        console.log(idx, el);
         var sub_time = parseFloat($(el).data('startTime'));
         if (sub_time < cTime)
             idx++;
-        console.log(idx, sub_time, cTime, sub_time < cTime);
+        // console.log(idx, sub_time, cTime, sub_time < cTime);
       })
       active_transcript_line(idx);
     });
